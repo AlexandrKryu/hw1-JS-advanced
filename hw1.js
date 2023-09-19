a3d39ec91f6d73a1200703ec96c3aab2a86fe5c6
AlexandrKryu <a.kryu84@gmail.com>
2023-09-19 19:29:28 +0300

    add hw1 advanced

---
+++ hw1.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// task1
+// const idElem = document.getElementById("super_link");
+// console.log(idElem);
+
+// task2
+// const elem = document.querySelectorAll('.card-link');
+// elem.forEach(element => element.textContent = "ссылка");
+// console.log(elem);
+
+// task3
+// const elem = document.querySelectorAll(".card-body .card-link");
+// console.log(elem);
+
+// task4
+// const elem = document.querySelector('[data-number="50"]');
+// console.log(elem);
+
+// task5
+// const elem = document.querySelector('title');
+// console.log(elem);
+
+// task6
+// const elem = document.querySelector('.card-title');
+// console.log(elem.parentElement);
+
+// task6
+// const text = document.createElement("p");
+// text.textContent ="Привет";
+// const elem = document.querySelector('.card');
+// elem.prepend(text);
+
+// task6
+// const elem = document.querySelector('h6');
+// elem.parentNode.removeChild(elem);
